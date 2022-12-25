07b61cba23fbeed5e89ef9ebbe4b4cdceca1a61e
Ilya <ilya.w.a.j@gmail.com>
2022-12-26 02:21:07 +0400

    upload post , delete post, edit post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -10,6 +10,8 @@ import CommentIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import styles from './Post.module.scss';
 import { UserInfo } from '../UserInfo';
 import { PostSkeleton } from './Skeleton';
+import { useDispatch } from 'react-redux';
+import { fetchRemovePost } from '../../redux/slices/posts';
 
 export const Post = ({
   id,
@@ -25,11 +27,18 @@ export const Post = ({
   isLoading,
   isEditable,
 }) => {
+
+  const dispatch = useDispatch()
+
   if (isLoading) {
     return <PostSkeleton />;
   }
 
-  const onClickRemove = () => {};
+  const onClickRemove = () => {
+    if(window.confirm('вы хотите удалить этот пост?')){
+    dispatch(fetchRemovePost(id))
+    }
+  };
 
   return (
     <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -10,6 +10,11 @@ export const fetchTags = createAsyncThunk('posts/fetchTags', async ()=>{
     return data
 })
 
+export const fetchRemovePost = createAsyncThunk('posts/fetchRemovePost', async (id)=>{
+    const { data }  = await axios.delete(`/posts/${id}`)
+    return data
+})
+
 const initialState = {
     posts:{
         items:[],
@@ -50,6 +55,9 @@ const postsSlice = createSlice({
             state.tags.items = [];
             state.tags.status = 'error'
         },
+        [fetchRemovePost.pending]: (state,action)=>{
+            state.posts.items = state.posts.items.filter(obj => obj._id !== action.meta.arg);
+        }
     }
 })
 
